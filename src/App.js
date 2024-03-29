6fed9399b8a2f43845f9d11015410dece07c4497
Eugene <30446748+EugeneZakhno@users.noreply.github.com>
2024-03-30 00:00:32 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,100 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./styles/main.css";
+import pic1 from "./img/1.jpg"
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div className="main-wrapper">
+
+            <div className="swiper slider slider1">
+                <div className="swiper-wrapper slider__wrapper">
+                    <div className="swiper-slide slider__item">
+                        <div className="slider__img"/> <img src={pic1}  alt="Link"/>
+                    </div>
+                    <div className="swiper-slide slider__item">
+                        <div className="slider__img"/> <img src="./img/2.jpg" alt="Link"/>
+                    </div>
+                    <div className="swiper-slide slider__item">
+                        <div className="slider__img"/> <img src="./img/3.jpg" alt="Link"/>
+                    </div>
+                    <div className="swiper-slide slider__item">
+                        <div className="slider__img"/> <img src="./img/4.jpg" alt="Link"/>
+                    </div>
+                    <div className="swiper-slide slider__item">
+                        <div className="slider__img"/> <img src="./img/5.jpg" alt="Link"/>
+                    </div>
+                    <div className="swiper-slide slider__item">
+                        <div className="slider__img"/> <img src="./img/6.jpg" alt="Link"/>
+                    </div>
+                </div>
+            </div>
+            <div className="swiper slider slider2">
+                <div className="swiper-wrapper slider__wrapper">
+                    <div className="swiper-slide slider__item">
+                        <div className="slider__img"/> <img src="./img/7.jpg" alt="Link"/>
+                    </div>
+                    <div className="swiper-slide slider__item">
+                        <div className="slider__img"/> <img src="./img/8.jpg" alt="Link"/>
+                    </div>
+                    <div className="swiper-slide slider__item">
+                        <div className="slider__img"/> <img src="./img/9.jpg" alt="Link"/>
+                    </div>
+                    <div className="swiper-slide slider__item">
+                        <div className="slider__img"/> <img src="./img/10.jpg" alt="Link"/>
+                    </div>
+                    <div className="swiper-slide slider__item">
+                        <div className="slider__img"/> <img src="./img/11.jpg" alt="Link"/>
+                    </div>
+                    <div className="swiper-slide slider__item">
+                        <div className="slider__img"/> <img src="./img/12.jpg" alt="Link"/>
+                    </div>
+                </div>
+            </div>
+            <div className="swiper slider slider3">
+                <div className="swiper-wrapper slider__wrapper">
+                    <div className="swiper-slide slider__item">
+                        <div className="slider__img"/> <img src="./img/13.jpg" alt="Link"/>
+                    </div>
+                    <div className="swiper-slide slider__item">
+                        <div className="slider__img"/> <img src="./img/14.jpg" alt="Link"/>
+                    </div>
+                    <div className="swiper-slide slider__item">
+                        <div className="slider__img"/> <img src="./img/15.jpg" alt="Link"/>
+                    </div>
+                    <div className="swiper-slide slider__item">
+                        <div className="slider__img"/> <img src="./img/16.jpg" alt="Link"/>
+                    </div>
+                    <div className="swiper-slide slider__item">
+                        <div className="slider__img"/> <img src="./img/17.jpg" alt="Link"/>
+                    </div>
+                    <div className="swiper-slide slider__item">
+                        <div className="slider__img"/> <img src="./img/18.jpg" alt="Link"/>
+                    </div>
+                </div>
+            </div>
+            <div className="swiper slider slider4">
+                <div className="swiper-wrapper slider__wrapper">
+                    <div className="swiper-slide slider__item">
+                        <div className="slider__img"/> <img src="./img/19.jpg" alt="Link"/>
+                    </div>
+                    <div className="swiper-slide slider__item">
+                        <div className="slider__img"/> <img src="./img/20.jpg" alt="Link"/>
+                    </div>
+                    <div className="swiper-slide slider__item">
+                        <div className="slider__img"/> <img src="./img/21.jpg" alt="Link"/>
+                    </div>
+                    <div className="swiper-slide slider__item">
+                        <div className="slider__img"/> <img src="./img/22.jpg" alt="Link"/>
+                    </div>
+                    <div className="swiper-slide slider__item">
+                        <div className="slider__img"/> <img src="./img/23.jpg" alt="Link"/>
+                    </div>
+                    <div className="swiper-slide slider__item">
+                        <div className="slider__img"/> <img src="./img/24.jpg" alt="Link"/>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
   );
 }
